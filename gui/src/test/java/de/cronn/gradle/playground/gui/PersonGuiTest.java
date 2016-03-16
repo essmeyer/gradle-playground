3795172bb70ebd5051e436e64c4d9e51cdddade0
essmeyer <jobst.essmeyer@cronn.de>
2016-03-16 15:39:44 +0100

    add working jacocoMerge integrated with sonarqube

--- gui/src/test/java/de/cronn/gradle/playground/gui/PersonGuiTest.java
+++ gui/src/test/java/de/cronn/gradle/playground/gui/PersonGuiTest.java
@@ -2,14 +2,24 @@ package de.cronn.gradle.playground.gui;
 
 import org.junit.Test;
 
+import de.cronn.gradle.playground.Person;
 import de.cronn.gradle.playground.gui.PersonGui;
 
 import static org.junit.Assert.*;
+
+import java.util.Arrays;
 
 public class PersonGuiTest {
 	@Test
 	public void testSomeLibraryMethod() {
 		PersonGui classUnderTest = new PersonGui();
 		assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+	}
+	
+	@Test
+	public void testPerson() throws Exception {
+		Person p = new Person("essmeyer");
+		p.setFirstNames(Arrays.asList("Jobst"));
+		assertNotNull(p.getFirstNames());
 	}
 }
--- service/src/test/java/de/cronn/gradle/playground/service/PersonServiceTest.java
+++ service/src/test/java/de/cronn/gradle/playground/service/PersonServiceTest.java
@@ -1,10 +1,11 @@
 package de.cronn.gradle.playground.service;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
 import org.junit.Test;
 
-import de.cronn.gradle.playground.service.PersonService;
-
-import static org.junit.Assert.*;
+import de.cronn.gradle.playground.Person;
 
 public class PersonServiceTest {
 	@Test
@@ -12,4 +13,23 @@ public class PersonServiceTest {
 		PersonService classUnderTest = new PersonServiceInMemoryImpl();
 		assertNull(classUnderTest.find("foo"));
 	}
+
+	@Test
+	public void coverPersonInDifferentProject() {
+		Person person = new Person("essmeyer");
+		
+		assertNotNull(person.getName());
+		person.setName(null);
+		assertNull(person.getName());
+		assertNotNull(person.getFirstNames());
+	}
+	
+	@Test
+	public void testDelete() {
+		PersonService classUnderTest = new PersonServiceInMemoryImpl().add(new Person("Essmeyer"));
+		Person deletedPerson = classUnderTest.delete("Essmeyer");
+		assertNotNull(deletedPerson);
+		assertNotNull(deletedPerson.getFirstNames());
+	}
+
 }
